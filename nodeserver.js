6df4ff69b204b5c28561f676e7cf2773de82eca8
UGADHI A <77853496+ugadhi@users.noreply.github.com>
2021-10-31 18:40:57 +0530

    nodejs

--- nodeserver.js
+++ nodeserver.js
@@ -1,6 +1,6 @@
 const http = require('http');
 http.createServer(function (req, res) {
     console.log("Server Started");
-    res.write(`Dhananjayan Durai : ${new Date()}`);
+    res.write(`ugadhi : ${new Date()}`);
     res.end();
 }).listen(8080);
